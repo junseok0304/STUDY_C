d5473b2a2cb76405b22b837bc387aacb082b9de4
윤준석 <83647215+junseok0304@users.noreply.github.com>
2022-03-14 14:59:34 +0900

    Create junseok01.c

---
+++ junseok01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main(){
+
+printf("* 프로그래밍 언어의 분류\n");
+printf("  - 기계어(machine language)\n");
+printf("  - 어셈블리어(assembly language)\n");
+printf("  - 고급 언어(high-level language)\n");
+
+}
