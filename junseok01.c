da11e8f251649572260827617a9e6c1e2c87734c
윤준석 <83647215+junseok0304@users.noreply.github.com>
2022-03-14 15:35:52 +0900

    Update junseok01.c

--- junseok01.c
+++ junseok01.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-main(){
-
-printf("* 프로그래밍 언어의 분류\n");
-printf("  - 기계어(machine language)\n");
-printf("  - 어셈블리어(assembly language)\n");
-printf("  - 고급 언어(high-level language)\n");
-
+int main(void){
+  printf("* 프로그래밍 언어의 분류\n");
+  printf("  - 기계어(machine language)\n");
+  printf("  - 어셈블리어(assembly language)\n");
+  printf("  - 고급 언어(high-level language)\n");
+  return 0;
 }
